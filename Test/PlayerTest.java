84123759d0bb6fe1d10955e853b1433b8772855d
DESKTOP-9AP00PL\user <orihy@gmail.com>
2023-05-14 11:26:25 +0900

    테스트 케이스 추가

---
+++ Test/PlayerTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void isDead() {
+    }
+}
